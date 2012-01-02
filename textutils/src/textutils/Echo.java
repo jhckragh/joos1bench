1f999cee7c8cbf13db7f351a7d0642190ec11420
Jacob H.C. Kragh <jhckragh@gmail.com>
2012-01-02 21:57:22 +0100

    Move text utilities to a separate directory

--- textutils/src/textutils/Echo.java
+++ textutils/src/textutils/Echo.java
@@ -1,3 +1,5 @@
+package textutils;
+
 public class Echo {
     public static void main(String[] args) {
         boolean printNewline = true;
--- textutils/src/textutils/Translate.java
+++ textutils/src/textutils/Translate.java
@@ -1,3 +1,5 @@
+package textutils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;

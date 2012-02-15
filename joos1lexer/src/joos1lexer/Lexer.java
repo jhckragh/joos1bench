057fbeb9edd42a1d8a2a6b643b58aff72e68a3ad
Jacob H.C. Kragh <jhckragh@gmail.com>
2012-02-15 21:58:09 +0100

    joos1lexer: Add support for keywords

--- joos1lexer/src/joos1lexer/Lexer.java
+++ joos1lexer/src/joos1lexer/Lexer.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.Reader;
+import java.util.Map;
+import java.util.HashMap;
 
 public final class Lexer {
     protected int columnNumber;
     protected TokenConstants constants;
     protected int currentChar;
     protected PushbackReader input;
+    protected Map keywords;
     protected int lineNumber;
 
     public Lexer(Reader in) {
@@ -17,6 +20,7 @@ public final class Lexer {
             in = new BufferedReader(in);
         input = new PushbackReader(in);
         constants = new TokenConstants();
+        keywords = createKeywordMap(constants);
         currentChar = -1;
         lineNumber = 1;
         columnNumber = -1;
@@ -43,7 +47,7 @@ public final class Lexer {
         int line = lineNumber;
         int column = columnNumber;
 
-        // Identifiers
+        // Identifiers, keywords, boolean literals, and null literal
         if (Character.isJavaIdentifierStart((char) currentChar)) {
             StringBuffer sb = new StringBuffer();
             sb.append((char) currentChar);
@@ -52,6 +56,9 @@ public final class Lexer {
                 sb.append((char) currentChar);
                 takeIt();
             }
+            Byte kw = (Byte) keywords.get((Object) sb.toString());
+            if (kw != null)
+                return new Token(kw.byteValue(), sb.toString(), line, column);
             return new Token(constants.IDENTIFIER, sb.toString(), line, column);
         }
 
@@ -189,5 +196,61 @@ public final class Lexer {
             lineNumber = lineNumber + 1;
             columnNumber = 0;
         }
+    }
+
+    protected Map createKeywordMap(TokenConstants constants) {
+        Map map = new HashMap();
+        map.put((Object) "abstract", (Object) new Byte(constants.ABSTRACT));
+        map.put((Object) "boolean", (Object) new Byte(constants.BOOLEAN));
+        map.put((Object) "break", (Object) new Byte(constants.BREAK));
+        map.put((Object) "byte", (Object) new Byte(constants.BYTE));
+        map.put((Object) "case", (Object) new Byte(constants.CASE));
+        map.put((Object) "catch", (Object) new Byte(constants.CATCH));
+        map.put((Object) "char", (Object) new Byte(constants.CHAR));
+        map.put((Object) "class", (Object) new Byte(constants.CLASS));
+        map.put((Object) "const", (Object) new Byte(constants.CONST));
+        map.put((Object) "continue", (Object) new Byte(constants.CONTINUE));
+        map.put((Object) "default", (Object) new Byte(constants.DEFAULT));
+        map.put((Object) "do", (Object) new Byte(constants.DO));
+        map.put((Object) "double", (Object) new Byte(constants.DOUBLE));
+        map.put((Object) "else", (Object) new Byte(constants.ELSE));
+        map.put((Object) "extends", (Object) new Byte(constants.EXTENDS));
+        map.put((Object) "final", (Object) new Byte(constants.FINAL));
+        map.put((Object) "finally", (Object) new Byte(constants.FINALLY));
+        map.put((Object) "float", (Object) new Byte(constants.FLOAT));
+        map.put((Object) "for", (Object) new Byte(constants.FOR));
+        map.put((Object) "goto", (Object) new Byte(constants.GOTO));
+        map.put((Object) "if", (Object) new Byte(constants.IF));
+        map.put((Object) "implements", (Object) new Byte(constants.IMPLEMENTS));
+        map.put((Object) "import", (Object) new Byte(constants.IMPORT));
+        map.put((Object) "instanceof", (Object) new Byte(constants.INSTANCEOF));
+        map.put((Object) "int", (Object) new Byte(constants.INT));
+        map.put((Object) "interface", (Object) new Byte(constants.INTERFACE));
+        map.put((Object) "long", (Object) new Byte(constants.LONG));
+        map.put((Object) "native", (Object) new Byte(constants.NATIVE));
+        map.put((Object) "new", (Object) new Byte(constants.NEW));
+        map.put((Object) "package", (Object) new Byte(constants.PACKAGE));
+        map.put((Object) "private", (Object) new Byte(constants.PRIVATE));
+        map.put((Object) "protected", (Object) new Byte(constants.PROTECTED));
+        map.put((Object) "public", (Object) new Byte(constants.PUBLIC));
+        map.put((Object) "return", (Object) new Byte(constants.RETURN));
+        map.put((Object) "short", (Object) new Byte(constants.SHORT));
+        map.put((Object) "static", (Object) new Byte(constants.STATIC));
+        map.put((Object) "strictfp", (Object) new Byte(constants.STRICTFP));
+        map.put((Object) "super", (Object) new Byte(constants.SUPER));
+        map.put((Object) "switch", (Object) new Byte(constants.SWITCH));
+        map.put((Object) "synchronized", (Object) new Byte(constants.SYNCHRONIZED));
+        map.put((Object) "this", (Object) new Byte(constants.THIS));
+        map.put((Object) "throw", (Object) new Byte(constants.THROW));
+        map.put((Object) "throws", (Object) new Byte(constants.THROWS));
+        map.put((Object) "transient", (Object) new Byte(constants.TRANSIENT));
+        map.put((Object) "try", (Object) new Byte(constants.TRY));
+        map.put((Object) "void", (Object) new Byte(constants.VOID));
+        map.put((Object) "volatile", (Object) new Byte(constants.VOLATILE));
+        map.put((Object) "while", (Object) new Byte(constants.WHILE));
+        map.put((Object) "true", (Object) new Byte(constants.TRUE));
+        map.put((Object) "false", (Object) new Byte(constants.FALSE));
+        map.put((Object) "null", (Object) new Byte(constants.NULL));
+        return map;
     }
 }
--- joos1lexer/test/joos1lexer/LexerTest.java
+++ joos1lexer/test/joos1lexer/LexerTest.java
@@ -293,6 +293,324 @@ public class LexerTest extends TestCase {
         assertByteEquals(constants.EOT, lexer.nextToken().kind());
     }
 
+    public void testKeywords() throws Exception {
+        String input = "abstract boolean break byte case catch char " +
+            "class const continue default do double else extends final " +
+            "finally float for goto if implements import instanceof int " +
+            "interface long native new package private protected public " +
+            "return short static strictfp super switch synchronized this " +
+            "throw throws transient try void volatile while true false null";
+        Lexer lexer = new Lexer((Reader) new StringReader(input));
+
+        Token _abstract = lexer.nextToken();
+        assertByteEquals(constants.ABSTRACT, _abstract.kind());
+        assertStringEquals("abstract", _abstract.text());
+        assertIntEquals(1, _abstract.line());
+        assertIntEquals(0, _abstract.column());
+
+        Token _boolean = lexer.nextToken();
+        assertByteEquals(constants.BOOLEAN, _boolean.kind());
+        assertStringEquals("boolean", _boolean.text());
+        assertIntEquals(1, _boolean.line());
+        assertIntEquals(9, _boolean.column());
+
+        Token _break = lexer.nextToken();
+        assertByteEquals(constants.BREAK, _break.kind());
+        assertStringEquals("break", _break.text());
+        assertIntEquals(1, _break.line());
+        assertIntEquals(17, _break.column());
+
+        Token _byte = lexer.nextToken();
+        assertByteEquals(constants.BYTE, _byte.kind());
+        assertStringEquals("byte", _byte.text());
+        assertIntEquals(1, _byte.line());
+        assertIntEquals(23, _byte.column());
+
+        Token _case = lexer.nextToken();
+        assertByteEquals(constants.CASE, _case.kind());
+        assertStringEquals("case", _case.text());
+        assertIntEquals(1, _case.line());
+        assertIntEquals(28, _case.column());
+
+        Token _catch = lexer.nextToken();
+        assertByteEquals(constants.CATCH, _catch.kind());
+        assertStringEquals("catch", _catch.text());
+        assertIntEquals(1, _catch.line());
+        assertIntEquals(33, _catch.column());
+
+        Token _char = lexer.nextToken();
+        assertByteEquals(constants.CHAR, _char.kind());
+        assertStringEquals("char", _char.text());
+        assertIntEquals(1, _char.line());
+        assertIntEquals(39, _char.column());
+
+        Token _class = lexer.nextToken();
+        assertByteEquals(constants.CLASS, _class.kind());
+        assertStringEquals("class", _class.text());
+        assertIntEquals(1, _class.line());
+        assertIntEquals(44, _class.column());
+
+        Token _const = lexer.nextToken();
+        assertByteEquals(constants.CONST, _const.kind());
+        assertStringEquals("const", _const.text());
+        assertIntEquals(1, _const.line());
+        assertIntEquals(50, _const.column());
+
+        Token _continue = lexer.nextToken();
+        assertByteEquals(constants.CONTINUE, _continue.kind());
+        assertStringEquals("continue", _continue.text());
+        assertIntEquals(1, _continue.line());
+        assertIntEquals(56, _continue.column());
+
+        Token _default = lexer.nextToken();
+        assertByteEquals(constants.DEFAULT, _default.kind());
+        assertStringEquals("default", _default.text());
+        assertIntEquals(1, _default.line());
+        assertIntEquals(65, _default.column());
+
+        Token _do = lexer.nextToken();
+        assertByteEquals(constants.DO, _do.kind());
+        assertStringEquals("do", _do.text());
+        assertIntEquals(1, _do.line());
+        assertIntEquals(73, _do.column());
+
+        Token _double = lexer.nextToken();
+        assertByteEquals(constants.DOUBLE, _double.kind());
+        assertStringEquals("double", _double.text());
+        assertIntEquals(1, _double.line());
+        assertIntEquals(76, _double.column());
+
+        Token _else = lexer.nextToken();
+        assertByteEquals(constants.ELSE, _else.kind());
+        assertStringEquals("else", _else.text());
+        assertIntEquals(1, _else.line());
+        assertIntEquals(83, _else.column());
+
+        Token _extends = lexer.nextToken();
+        assertByteEquals(constants.EXTENDS, _extends.kind());
+        assertStringEquals("extends", _extends.text());
+        assertIntEquals(1, _extends.line());
+        assertIntEquals(88, _extends.column());
+
+        Token _final = lexer.nextToken();
+        assertByteEquals(constants.FINAL, _final.kind());
+        assertStringEquals("final", _final.text());
+        assertIntEquals(1, _final.line());
+        assertIntEquals(96, _final.column());
+
+        Token _finally = lexer.nextToken();
+        assertByteEquals(constants.FINALLY, _finally.kind());
+        assertStringEquals("finally", _finally.text());
+        assertIntEquals(1, _finally.line());
+        assertIntEquals(102, _finally.column());
+
+        Token _float = lexer.nextToken();
+        assertByteEquals(constants.FLOAT, _float.kind());
+        assertStringEquals("float", _float.text());
+        assertIntEquals(1, _float.line());
+        assertIntEquals(110, _float.column());
+
+        Token _for = lexer.nextToken();
+        assertByteEquals(constants.FOR, _for.kind());
+        assertStringEquals("for", _for.text());
+        assertIntEquals(1, _for.line());
+        assertIntEquals(116, _for.column());
+
+        Token _goto = lexer.nextToken();
+        assertByteEquals(constants.GOTO, _goto.kind());
+        assertStringEquals("goto", _goto.text());
+        assertIntEquals(1, _goto.line());
+        assertIntEquals(120, _goto.column());
+
+        Token _if = lexer.nextToken();
+        assertByteEquals(constants.IF, _if.kind());
+        assertStringEquals("if", _if.text());
+        assertIntEquals(1, _if.line());
+        assertIntEquals(125, _if.column());
+
+        Token _implements = lexer.nextToken();
+        assertByteEquals(constants.IMPLEMENTS, _implements.kind());
+        assertStringEquals("implements", _implements.text());
+        assertIntEquals(1, _implements.line());
+        assertIntEquals(128, _implements.column());
+
+        Token _import = lexer.nextToken();
+        assertByteEquals(constants.IMPORT, _import.kind());
+        assertStringEquals("import", _import.text());
+        assertIntEquals(1, _import.line());
+        assertIntEquals(139, _import.column());
+
+        Token _instanceof = lexer.nextToken();
+        assertByteEquals(constants.INSTANCEOF, _instanceof.kind());
+        assertStringEquals("instanceof", _instanceof.text());
+        assertIntEquals(1, _instanceof.line());
+        assertIntEquals(146, _instanceof.column());
+
+        Token _int = lexer.nextToken();
+        assertByteEquals(constants.INT, _int.kind());
+        assertStringEquals("int", _int.text());
+        assertIntEquals(1, _int.line());
+        assertIntEquals(157, _int.column());
+
+        Token _interface = lexer.nextToken();
+        assertByteEquals(constants.INTERFACE, _interface.kind());
+        assertStringEquals("interface", _interface.text());
+        assertIntEquals(1, _interface.line());
+        assertIntEquals(161, _interface.column());
+
+        Token _long = lexer.nextToken();
+        assertByteEquals(constants.LONG, _long.kind());
+        assertStringEquals("long", _long.text());
+        assertIntEquals(1, _long.line());
+        assertIntEquals(171, _long.column());
+
+        Token _native = lexer.nextToken();
+        assertByteEquals(constants.NATIVE, _native.kind());
+        assertStringEquals("native", _native.text());
+        assertIntEquals(1, _native.line());
+        assertIntEquals(176, _native.column());
+
+        Token _new = lexer.nextToken();
+        assertByteEquals(constants.NEW, _new.kind());
+        assertStringEquals("new", _new.text());
+        assertIntEquals(1, _new.line());
+        assertIntEquals(183, _new.column());
+
+        Token _package = lexer.nextToken();
+        assertByteEquals(constants.PACKAGE, _package.kind());
+        assertStringEquals("package", _package.text());
+        assertIntEquals(1, _package.line());
+        assertIntEquals(187, _package.column());
+
+        Token _private = lexer.nextToken();
+        assertByteEquals(constants.PRIVATE, _private.kind());
+        assertStringEquals("private", _private.text());
+        assertIntEquals(1, _private.line());
+        assertIntEquals(195, _private.column());
+
+        Token _protected = lexer.nextToken();
+        assertByteEquals(constants.PROTECTED, _protected.kind());
+        assertStringEquals("protected", _protected.text());
+        assertIntEquals(1, _protected.line());
+        assertIntEquals(203, _protected.column());
+
+        Token _public = lexer.nextToken();
+        assertByteEquals(constants.PUBLIC, _public.kind());
+        assertStringEquals("public", _public.text());
+        assertIntEquals(1, _public.line());
+        assertIntEquals(213, _public.column());
+
+        Token _return = lexer.nextToken();
+        assertByteEquals(constants.RETURN, _return.kind());
+        assertStringEquals("return", _return.text());
+        assertIntEquals(1, _return.line());
+        assertIntEquals(220, _return.column());
+
+        Token _short = lexer.nextToken();
+        assertByteEquals(constants.SHORT, _short.kind());
+        assertStringEquals("short", _short.text());
+        assertIntEquals(1, _short.line());
+        assertIntEquals(227, _short.column());
+
+        Token _static = lexer.nextToken();
+        assertByteEquals(constants.STATIC, _static.kind());
+        assertStringEquals("static", _static.text());
+        assertIntEquals(1, _static.line());
+        assertIntEquals(233, _static.column());
+
+        Token _strictfp = lexer.nextToken();
+        assertByteEquals(constants.STRICTFP, _strictfp.kind());
+        assertStringEquals("strictfp", _strictfp.text());
+        assertIntEquals(1, _strictfp.line());
+        assertIntEquals(240, _strictfp.column());
+
+        Token _super = lexer.nextToken();
+        assertByteEquals(constants.SUPER, _super.kind());
+        assertStringEquals("super", _super.text());
+        assertIntEquals(1, _super.line());
+        assertIntEquals(249, _super.column());
+
+        Token _switch = lexer.nextToken();
+        assertByteEquals(constants.SWITCH, _switch.kind());
+        assertStringEquals("switch", _switch.text());
+        assertIntEquals(1, _switch.line());
+        assertIntEquals(255, _switch.column());
+
+        Token _synchronized = lexer.nextToken();
+        assertByteEquals(constants.SYNCHRONIZED, _synchronized.kind());
+        assertStringEquals("synchronized", _synchronized.text());
+        assertIntEquals(1, _synchronized.line());
+        assertIntEquals(262, _synchronized.column());
+
+        Token _this = lexer.nextToken();
+        assertByteEquals(constants.THIS, _this.kind());
+        assertStringEquals("this", _this.text());
+        assertIntEquals(1, _this.line());
+        assertIntEquals(275, _this.column());
+
+        Token _throw = lexer.nextToken();
+        assertByteEquals(constants.THROW, _throw.kind());
+        assertStringEquals("throw", _throw.text());
+        assertIntEquals(1, _throw.line());
+        assertIntEquals(280, _throw.column());
+
+        Token _throws = lexer.nextToken();
+        assertByteEquals(constants.THROWS, _throws.kind());
+        assertStringEquals("throws", _throws.text());
+        assertIntEquals(1, _throws.line());
+        assertIntEquals(286, _throws.column());
+
+        Token _transient = lexer.nextToken();
+        assertByteEquals(constants.TRANSIENT, _transient.kind());
+        assertStringEquals("transient", _transient.text());
+        assertIntEquals(1, _transient.line());
+        assertIntEquals(293, _transient.column());
+
+        Token _try = lexer.nextToken();
+        assertByteEquals(constants.TRY, _try.kind());
+        assertStringEquals("try", _try.text());
+        assertIntEquals(1, _try.line());
+        assertIntEquals(303, _try.column());
+
+        Token _void = lexer.nextToken();
+        assertByteEquals(constants.VOID, _void.kind());
+        assertStringEquals("void", _void.text());
+        assertIntEquals(1, _void.line());
+        assertIntEquals(307, _void.column());
+
+        Token _volatile = lexer.nextToken();
+        assertByteEquals(constants.VOLATILE, _volatile.kind());
+        assertStringEquals("volatile", _volatile.text());
+        assertIntEquals(1, _volatile.line());
+        assertIntEquals(312, _volatile.column());
+
+        Token _while = lexer.nextToken();
+        assertByteEquals(constants.WHILE, _while.kind());
+        assertStringEquals("while", _while.text());
+        assertIntEquals(1, _while.line());
+        assertIntEquals(321, _while.column());
+
+        Token _true = lexer.nextToken();
+        assertByteEquals(constants.TRUE, _true.kind());
+        assertStringEquals("true", _true.text());
+        assertIntEquals(1, _true.line());
+        assertIntEquals(327, _true.column());
+
+        Token _false = lexer.nextToken();
+        assertByteEquals(constants.FALSE, _false.kind());
+        assertStringEquals("false", _false.text());
+        assertIntEquals(1, _false.line());
+        assertIntEquals(332, _false.column());
+
+        Token _null = lexer.nextToken();
+        assertByteEquals(constants.NULL, _null.kind());
+        assertStringEquals("null", _null.text());
+        assertIntEquals(1, _null.line());
+        assertIntEquals(338, _null.column());
+
+        assertByteEquals(constants.EOT, lexer.nextToken().kind());
+    }
+
     // public void testMinimal() throws Exception {
     //     String input = "public class Minimal {}";
     //     Lexer lexer = new Lexer((Reader) new StringReader(input));
